497b8a5bde684cc2b735cf55f2dba46fda966cd0
hansnavarrete1602 <53948703+hansnavarrete1602@users.noreply.github.com>
2022-06-10 10:17:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+function printInput(){
+  if (input.value.substr(-1) === "a" || input.value.substr(-1) === "A") {
+    output.innerHTML = "El nombre "+input.value+" es de una Mujer";
+  }
+  else{
+    output.innerHTML = "El nombre "+input.value+" es de una Hombre";
+  }
+}
+
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("button").click();
+  }
+
+});
